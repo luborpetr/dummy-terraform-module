aeca2fd9839a134d80ca3417f16e305b0d4d2a62
Lubor PETR <lubor.petr@thalesgroup.com>
2022-03-08 11:40:12 +0100

    Remove zip archive

--- main.tf
+++ main.tf
@@ -17,21 +17,3 @@ resource "random_id" "server" {
 
   byte_length = 8
 }
-
-##
-# This is covering scenarios when a module generates artifacts in terraform plan stage
-##
-data "archive_file" "archive_foo" {
-  type        = "zip"
-  source_dir = "${path.root}/dummy_data"
-  output_path = "${path.root}/dummy_data.zip"
-}
-
-##
-# Consume artifact created in terraform plan stage
-##
-resource "null_resource" "extract_my_tgz" {
-  provisioner "local-exec" {
-    command = "ls -lah ${path.root}/dummy_data.zip"
-  }
-}
